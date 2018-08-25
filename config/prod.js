bc61d37c0c2c5c52cab73f0d6b4614b7f0ca1c9c
Benjamin Joyce <benjaminandrewjoyce@gmail.com>
2018-08-25 13:57:49 +1000

    make better

--- config/prod.js
+++ config/prod.js
@@ -2,12 +2,8 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-<<<<<<< HEAD
     cookieKey: process.env.COOKIE_KEY,
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     stripeSecretKey: process.env.STRIPE_SECRET_KEY
 
-=======
-    cookieKey: process.env.COOKIE_KEY
->>>>>>> a0f247bb54738a5d0df678167a3c1c2d462b077e
 };
