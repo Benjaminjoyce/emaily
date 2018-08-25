6d31308809f4100f6fbdbd2b4abc40eae6e8b836
Benjamin Joyce <benjaminandrewjoyce@gmail.com>
2018-08-25 13:43:07 +1000

    make better

--- routes/landingPage.js
+++ routes/landingPage.js
@@ -1,3 +1,5 @@
-app.get('/', (req, res) => {
-    res.send('LANDING PAGE IS HERE')
-});
+module.exports = (app) => {
+    app.get('/', (req, res) => {
+        res.send('LANDING PAGE IS HERE')
+    })
+}
